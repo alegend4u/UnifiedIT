a5a48eb4ac99e9a279e3474dd2c038634e5aacc7
Rishabh Prajapati <rishabh.prajapati0@gmail.com>
2019-04-16 14:57:52 +0530

    Admin Panel and Custom Authentication implemented.

---
+++ UnifiedIT/Accountant/database_settings/itsinsname.py
@@ -0,0 +1,9 @@
+
+DATABASES['itsinsname'] = {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'itsinsname',    
+'USER': 'postgres',    
+'PASSWORD': 'postgres', 
+'HOST': 'localhost',         
+'PORT': '5432',         
+} 
