ad4264c672c1ddb4d311e00529d425f703af6da8
Rishabh Prajapati <rishabh.prajapati0@gmail.com>
2019-06-03 11:05:20 +0530

    Account.models updated.

--- UnifiedIT/Profiler/models.py
+++ UnifiedIT/Profiler/models.py
@@ -46,13 +46,12 @@ class Contact(models.Model):
         return self.value
 
 
-class Faculty(models.Model):
+class Faculty(Person):
     designation = models.CharField(max_length=MAX_LENGTH)
     qualification = models.CharField(max_length=MAX_LENGTH)
 
 
-class Student(models.Model):
-    sid = models.CharField(max_length=MAX_LENGTH)
+class Student(Person):
     roll_no = models.IntegerField()
     semester = models.IntegerField()
     admission_type = models.CharField(max_length=MAX_LENGTH)
@@ -63,3 +62,81 @@ class Student(models.Model):
     blood_group = models.CharField(max_length=MAX_LENGTH)
     guardian_name = models.CharField(max_length=MAX_LENGTH)
     guardian_relation = models.CharField(max_length=MAX_LENGTH)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+    semester = models.CharField(max_length=MAX_LENGTH)
+    credits = models.CharField(max_length=MAX_LENGTH)
+    description = models.CharField(max_length=MAX_LENGTH)
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+    duration = models.CharField(max_length=MAX_LENGTH)
+    description = models.CharField(max_length=MAX_LENGTH)
+
+
+class Division(models.Model):
+    label = models.CharField(max_length=MAX_LENGTH)
+    semester = models.CharField(max_length=MAX_LENGTH)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+    dept_code = models.CharField(max_length=MAX_LENGTH)
+
+
+class Batch(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+
+
+class Exam(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+    theory_prac = models.CharField(max_length=MAX_LENGTH)
+    isRemedial = models.CharField(max_length=MAX_LENGTH)
+
+
+class Grade(models.Model):
+    score = models.CharField(max_length=MAX_LENGTH)
+    max_marks = models.CharField(max_length=MAX_LENGTH)
+    duration = models.CharField(max_length=MAX_LENGTH)
+    exam_data = models.CharField(max_length=MAX_LENGTH)
+
+
+class Attendance(models.Model):
+    status = models.CharField(max_length=MAX_LENGTH)
+    date = models.CharField(max_length=MAX_LENGTH)
+
+
+class TimeTable(models.Model):
+    dat = models.CharField(max_length=MAX_LENGTH)
+    seq_no = models.CharField(max_length=MAX_LENGTH)
+    start_time = models.CharField(max_length=MAX_LENGTH)
+    location = models.CharField(max_length=MAX_LENGTH)
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=MAX_LENGTH)
+    description = models.CharField(max_length=MAX_LENGTH)
+
+
+class Session(models.Model):
+    is_theory = models.CharField(max_length=MAX_LENGTH)
+    duration = models.CharField(max_length=MAX_LENGTH)
+
+
+class ToBeCovered(models.Model):
+    date = models.CharField(max_length=MAX_LENGTH)
+
+
+class SessTheory(models.Model):
+    faculty = models.ForeignKey('Faculty', on_delete=models.CASCADE)
+
+
+class FacultyIncharge(models.Model):
+    pass
+
+
+class SessPrac(models.Model):
+    pass
